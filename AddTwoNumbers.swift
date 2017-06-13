008d8c6d1160ff87c6a148dcde58d9836661c0c6
Ishan Sharma <ishan.sharma091@gmail.com>
2017-06-13 12:47:11 -0400

    Create AddTwoNumbers.swift

---
+++ AddTwoNumbers.swift
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class AddTwoNumbers {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var carry = 0
+        var node1 = l1
+        var node2 = l2
+        var prev = ListNode(0)
+        var head = prev
+        while node1 != nil || node2 != nil || carry != 0 {
+            var current = ListNode(0)
+            var sum = (node1 == nil ? 0 : node1!.val) + (node2 == nil ? 0 : node2!.val) + carry
+            carry = sum / 10
+            current.val = sum % 10
+            prev.next = current
+            prev = current
+            node1 = (node1 == nil) ? nil : node1!.next
+            node2 = (node2 == nil) ? nil : node2!.next
+        }
+        return head.next
+    }
+}
